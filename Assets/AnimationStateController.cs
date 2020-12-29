cc573e06edda2e621bbe2749ce3362000cca8d4e
Leon Schauer <leon@nyoucomer.de>
2020-12-29 18:47:32 +0100

    Started movement animation blending

---
+++ Assets/AnimationStateController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+    int isRunningHash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool forwardPressed = Input.GetKey("w");
+        bool runPressed = Input.GetKey("left shift");
+        
+        //Walking
+        if (!isWalking && forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, true);
+        }
+        if (isWalking && !forwardPressed)
+        {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+
+
+
+        //Running
+        if (!isRunning && (isWalking && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+        if (isRunning && (!isWalking || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+}

b69fdf27b9d10cf3331c06fdd493d3a7d8665a52
Tiffin007 <s-leischmidt@web.de>
2021-01-07 22:45:19 +0100

    added sprint

--- Assets/Scripts/ThirdPersonMovemnet.cs
+++ Assets/Scripts/ThirdPersonMovemnet.cs
@@ -7,7 +7,8 @@ public class ThirdPersonMovemnet : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
-    public float speed = 6f;
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 12f;
     [SerializeField] float strifeSpeed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
@@ -49,18 +50,29 @@ public class ThirdPersonMovemnet : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            walkSpeed = sprintSpeed;
+        }
+        else
+        {
+            walkSpeed = walkSpeed;
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            walkSpeed = 4f;
+        }
+
+
+
         if(direction.magnitude >= 0.1f)
         {
-            
 
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
                 transform.rotation = Quaternion.Euler(0f, angle, 0f);
                 Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-                controller.Move(moveDir.normalized * speed * Time.deltaTime);
-
-            
-                
+                controller.Move(moveDir.normalized * walkSpeed * Time.deltaTime);                
            
         }
 
